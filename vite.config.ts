02137c4a0f548facd7ab97dac6e17191df819534
Mr.Lee <chzxxuanzheng@gmail.com>
2025-11-13 12:14:22 +0800

    feat: 优化qface拷贝，减少大小。同时增加直接使用koishi表情的选项

--- vite.config.ts
+++ vite.config.ts
@@ -2,69 +2,101 @@ import ViteYaml from '@modyfi/vite-plugin-yaml'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-import { resolve } from 'path'
+import { resolve } from 'node:path'
 import { visualizer } from 'rollup-plugin-visualizer'
-import { defineConfig, type PluginOption } from 'vite'
+import { defineConfig, loadEnv, type PluginOption } from 'vite'
 import { VitePWA } from 'vite-plugin-pwa'
 import { viteStaticCopy } from 'vite-plugin-static-copy'
+import qfaceInfo from './src/renderer/src/assets/img/qq-face/public/assets/qq_emoji/_index.json' with { type: 'json' }
 
 // https://vite.dev/config/
 /** @type {import('vite').UserConfig} */
-export default defineConfig({
-    root: './src/renderer',
-    envDir: '../../',
-    base: './',
-    server: {
-        port: 8080,
-        proxy: {
-            '/api': {
-                target: 'http://localhost:3000',
-                changeOrigin: true,
-                rewrite: (path) => path.replace(/^\/api/, '')
-            },
-        },
-    },
-    plugins: [
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd())
+    const useLocalFace = env.VITE_LOCAL_FACE == 'true'
+
+    const plugins: PluginOption[] = [
         vue(),
         vueDevTools(),
         ViteYaml(),
         VitePWA({ registerType: 'autoUpdate' }),
         visualizer() as PluginOption,
-        viteStaticCopy({
-            targets: [
-                {
-                src: 'src/assets/img/qq-face/public/assets/qq_emoji/**/*',
-                dest: 'img/qqface',
-                },
-            ],
-        }),
-    ],
-    resolve: {
-        alias: {
-            '@renderer': resolve(__dirname, 'src/renderer/src'),
-            fs: 'rollup-plugin-node-polyfills/polyfills/empty',
+    ]
+
+    if (useLocalFace) {
+        const apngList: string[] = []
+        const lottieList: string[] = []
+        for (const info of qfaceInfo) {
+            for (const pathInfo of info.assets) {
+                if (pathInfo.type === 2)
+                    apngList.push(`src/assets/img/qq-face/public/${pathInfo.path}`)
+
+                else if (pathInfo.type === 3)
+                    lottieList.push(`src/assets/img/qq-face/public/${pathInfo.path}`)
+            }
         }
-    },
-    build: {
-        outDir: resolve(__dirname, 'dist'),
-        emptyOutDir: true,
-        chunkSizeWarningLimit: 1100,
-        rollupOptions: {
-            input: { main: resolve('src/renderer/index.html') },
-            external: [ resolve('src/renderer/src/assets/img/qq-face/docs') ],
-            onwarn: (warning) => {
-                if(warning.code === 'CIRCULAR_DEPENDENCY') return
+
+        const targets: any = []
+        for (const src of apngList) {
+            targets.push({
+                src: src,
+                dest: 'img/qface/',
+            })
+        }
+        for (const src of lottieList) {
+            targets.push({
+                src: src,
+                dest: 'img/qface/',
+            })
+        }
+
+        plugins.push(viteStaticCopy({
+            targets: targets
+        }))
+    }
+
+    return {
+        root: './src/renderer',
+        envDir: '../../',
+        base: './',
+        server: {
+            port: 8080,
+            proxy: {
+                '/api': {
+                    target: 'http://localhost:3000',
+                    changeOrigin: true,
+                    rewrite: (path) => path.replace(/^\/api/, '')
+                },
             },
-            output: {
-                chunkFileNames: 'assets/js/[name]-[hash].js',
-                entryFileNames: 'assets/js/[name]-[hash].js',
-                assetFileNames: 'assets/[ext]/[name]-[hash].[ext]',
-                manualChunks(id) {
-                    if (id.includes('node_modules')) {
-                        // 让每个插件都打包成独立的文件
-                        return id.toString().split('node_modules/')[1].split('/')[0].toString()
+        },
+        plugins: plugins,
+        resolve: {
+            alias: {
+                '@renderer': resolve(__dirname, 'src/renderer/src'),
+                fs: 'rollup-plugin-node-polyfills/polyfills/empty',
+            }
+        },
+        build: {
+            outDir: resolve(__dirname, 'dist'),
+            emptyOutDir: true,
+            chunkSizeWarningLimit: 1100,
+            rollupOptions: {
+                input: { main: resolve('src/renderer/index.html') },
+                external: [ resolve('src/renderer/src/assets/img/qq-face/docs') ],
+                onwarn: (warning) => {
+                    if(warning.code === 'CIRCULAR_DEPENDENCY') return
+                },
+                output: {
+                    chunkFileNames: 'assets/js/[name]-[hash].js',
+                    entryFileNames: 'assets/js/[name]-[hash].js',
+                    assetFileNames: 'assets/[ext]/[name]-[hash].[ext]',
+                    manualChunks(id) {
+                        if (id.includes('node_modules')) {
+                            // 让每个插件都打包成独立的文件
+                            return id.toString().split('node_modules/')[1].split('/')[0].toString()
+                        }
+                        return
                     }
-                    return
                 }
             }
         }
