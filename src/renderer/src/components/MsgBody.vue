0035364d57ee41d329223e8402b78d6a29af09b8
Mr.Lee <chzxxuanzheng@gmail.com>
2026-01-03 00:47:18 +0800

    feat: 优化MSgBody的额外信息交互体验

--- src/renderer/src/components/MsgBody.vue
+++ src/renderer/src/components/MsgBody.vue
@@ -361,7 +361,7 @@
             :src="data.sender.face"
             :alt="data.sender.name"
             @dblclick="$emit('senderDoubleClick', data.sender)">
-        <div v-if="data.emojis"
+        <div v-if="data.emojis && Object.keys(data.emojis).length > 0"
             class="emoji-like">
             <div class="emoji-space" />
             <div class="emoji-like-body">
@@ -381,14 +381,16 @@
         </div>
         <div class="message-ex-info">
             <div v-if="direction === 'right'" class="space" />
-            <template v-for="info in exInfo" :key="data.uuid + '-exinfo-' + info">
-                <a v-if="info === 'msgId'">
-                    msgId: {{ data.message_id }}
-                </a>
-                <a v-if="info === 'time'">
-                    time: {{ data.time?.format('year') }}
-                </a>
-            </template>
+            <div class="content">
+                <template v-for="info in exInfo" :key="data.uuid + '-exinfo-' + info">
+                    <a v-if="info === 'msgId'">
+                        msgId: {{ data.message_id }}
+                    </a>
+                    <a v-if="info === 'time'">
+                        time: {{ data.time?.format('year') }}
+                    </a>
+                </template>
+            </div>
         </div>
     </div>
 </template>
